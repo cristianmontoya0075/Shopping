d56c30d4659e248022fa64bd8e60bf3e2f254ca7
cristianmontoya0075 <cristianmontoya0075@gmail.com>
2022-06-03 10:46:16 -0500

    Begin States CRUD

--- Shopping1/Shopping1/Controllers/CountriesController.cs
+++ Shopping1/Shopping1/Controllers/CountriesController.cs
@@ -54,7 +54,8 @@ namespace Shopping1.Controllers
         [HttpGet]
         public IActionResult Create()
         {
-            return View();
+            Country country = new() {States = new List<State>() };
+            return View(country);
         }
 
         // POST: Countries/Create
@@ -247,6 +248,82 @@ namespace Shopping1.Controllers
             return View(model);
 
         }
+
+
+
+        // GET: Countries/Edit/5
+        public async Task<IActionResult> EditState(int? id)
+        {
+            if (id == null || _context.Countries == null)
+            {
+                return NotFound();
+            }
+
+            State state = await _context.States
+                .Include(s => s.Country)
+                .FirstOrDefaultAsync( s => s.Id == id);
+            if (state == null)
+            {
+                return NotFound();
+            }
+
+
+            StateViewModel model = new()
+            {
+                CountryId = state.Country.Id,
+                Id = state.Id,
+                Name = state.Name,
+            };
+
+            return View(model);
+        }
+
+        // POST: Countries/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditState(int id, StateViewModel model)
+        {
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    State state = new()
+                    { 
+                        Id = model.Id,
+                        Name = model.Name,
+
+                    
+                    };
+                    _context.Update(state);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Details), new {Id = model.CountryId});
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe un Departamento/estado con el mismo nombre en este país.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+            return View(model);
+        }
+
 
 
     }
